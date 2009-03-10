3d97086c584f03e350bb5f7cb21b8fd2e19963e3
Chris Dary <umbrae@gmail.com>
2009-03-10 21:33:55 +0000

    Accidentally removed stylesheet code block. Readding.

--- js/readability.js
+++ js/readability.js
@@ -88,6 +88,13 @@ function grabArticle() {
 	  topDiv.innerHTML = 'Sorry, readability was unable to parse this page for content. If you feel like it should have been able to, please <a href="http://code.google.com/p/arc90labs-readability/issues/entry">let us know by submitting an issue.</a>';
 	}
 	
+	// REMOVES ALL STYLESHEETS ...
+	for (var k=0;k < document.styleSheets.length; k++) {
+		if (document.styleSheets[k].href != null && document.styleSheets[k].href.lastIndexOf("readability") == -1) {
+			document.styleSheets[k].disabled = true;
+		}
+	}
+
 	// Remove all style tags in head (not doing this on IE) :
 	var styleTags = document.getElementsByTagName("style");
 	for (var j=0;j < styleTags.length; j++)
